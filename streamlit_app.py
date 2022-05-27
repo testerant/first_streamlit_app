813b2dc6c668aa55e371863f5818e151e582eea5
testerant <105889385+testerant@users.noreply.github.com>
2022-05-27 13:54:19 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -70,13 +70,11 @@ if streamlit.button('Add a Fruit to the List'):
    my_cnx.close()
    streamlit.text(back_from_function)
 
-# don't run anything past here while we troubleshoot
-streamlit.stop()
-
 import snowflake.connector
 
 # Allow the end user to add a fruit to the list
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 streamlit.write('Thanks for adding', add_my_fruit)
 
-
+# don't run anything past here while we troubleshoot
+streamlit.stop()
